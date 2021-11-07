2ffe0edead8211634d23f30a6876e2e796332dc8
lavaxcii <lavaxcii@gmail.com>
2021-11-07 11:37:23 +0100

    Add player container generator

--- appTTT.js
+++ appTTT.js
@@ -8,9 +8,11 @@ const PlayerGen = (name) => {
 
 const GridGen = (id) => {
   let playerSqr = false;
-  let aiSqr = false;
+  let player2Sqr = false;
+  let aiSqr = false; 
   return {
     playerSqr,
+    player2Sqr,
     aiSqr,
     id
   };
@@ -25,21 +27,51 @@ const gameBoard = (() => {
     gameBoardSqrs.push(GridGen(id))
   };
 
-  const removeMenu = () => {
+  const removeMenuStartGame = () => {
     startGameBtn.addEventListener('click', () => {
+      const form = document.querySelector('form');
       introMenu.style.left = '-50%';
       for (let i = 0; i < 3; i++) {
         const createDiv = document.createElement('div');
         createDiv.classList.add(`gameGrid${i}`);
         gameContainer.appendChild(createDiv);
       };
-      const boardGrid = document.querySelector('.gameGrid1');
-      for (let i = 1; i <= 9; i++) {
-        const createDiv = document.createElement('div');
-        createDiv.classList.add(`gridSqr${i}`);
-        boardGrid.appendChild(createDiv);
-        addSqrs(`${i}`);
-      };
+
+      const player1Name = document.querySelector('#player1Name');
+      const player2Name = document.querySelector('#player2Name');
+      const boardGrid = document.querySelectorAll('div');
+      boardGrid.forEach((divs) => {
+        if (divs.classList.contains('gameGrid0') || divs.classList.contains('gameGrid2')) {
+          for (let i = 1; i <= 2; i++) {
+            const createP = document.createElement('p');
+            createP.classList.add(`playerP${i}`);
+            divs.appendChild(createP);
+          }
+          const addName = divs.querySelector(`.playerP1`);
+          const addScore = divs.querySelector(`.playerP2`);
+          if (divs.classList.contains('gameGrid0')) {
+            addName.textContent = `${player1Name.value}`;
+            addScore.textContent = 0;
+          } else if (divs.classList.contains('gameGrid2')) {
+            addName.textContent = `${player2Name.value}`;
+            addScore.textContent = 0;
+            console.log(true);
+          };
+
+          const createRestartBtn = document.createElement('button');
+          createRestartBtn.classList.add('restartBtn');
+          divs.appendChild(createRestartBtn);
+          
+        } else if (divs.classList.contains('gameGrid1')) {
+          for (let i = 1; i <= 9; i++) {
+            const createDiv = document.createElement('div');
+            createDiv.classList.add(`sqr${i}`);
+            divs.appendChild(createDiv);
+            addSqrs(parseInt(`${i}`));
+          };
+        };
+      });
+      form.reset();
     });
   };
 
@@ -48,12 +80,17 @@ const gameBoard = (() => {
     players.push(PlayerGen(name));
   };
 
+  const gameLogic = () => {
+
+  }
+
   return {
-    removeMenu,
+    removeMenuStartGame,
     addPlayer,
     gameBoardSqrs,
     players
   };
 })();
-gameBoard.removeMenu();
+gameBoard.removeMenuStartGame();
 gameBoard.addPlayer('Jack');
+
