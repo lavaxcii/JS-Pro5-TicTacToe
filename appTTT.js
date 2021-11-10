1615582c1f48416599e317ef69e7f48eb7c478c4
lavaxcii <lavaxcii@gmail.com>
2021-11-10 10:19:11 +0100

    Add logic for reseting game board after round is finished

--- appTTT.js
+++ appTTT.js
@@ -9,7 +9,7 @@ const PlayerGen = (name) => {
 const GridGen = (id) => {
   let playerSqr = false;
   let player2Sqr = false;
-  let aiSqr = false; 
+  let aiSqr = false;
   let sqrStatusLocked = false;
   return {
     playerSqr,
@@ -24,7 +24,7 @@ const gameBoard = (() => {
   const gameContainer = document.querySelector('.gameContainer');
   const introMenu = document.querySelector('.introMenu');
   const startGameBtn = document.querySelector('.startGameBtn');
-  
+
   let gameBoardSqrs = [];
   const addSqrs = (id) => {
     gameBoardSqrs.push(GridGen(id))
@@ -38,9 +38,9 @@ const gameBoard = (() => {
   const initializeGame = () => {
     startGameBtn.addEventListener('click', () => {
       const userGameOptionChoice = document.querySelector('input[name="go"]:checked').value;
+      let remeberGameMode2 = '';
       removeMenu();
-      addPlayerAndGameGrid();
-      startGameMode(userGameOptionChoice);
+      addPlayerAndGameGrid(userGameOptionChoice);
       enableRestartGame();
     });
   };
@@ -62,7 +62,31 @@ const gameBoard = (() => {
     form.reset();
   };
 
-  const addPlayerAndGameGrid = () => {
+  const deleteGameGrid = (divs) => {
+    divs.remove();
+  };
+
+  const createGameGrid = (divs, userGameOptionChoice) => {
+    for (let i = 1; i <= 9; i++) {
+      const createDiv = document.createElement('div');
+      createDiv.classList.add(`sqr${i}`);
+      divs.appendChild(createDiv);
+      console.log('Push to array!')
+      addSqrs(`sqr${i}`);
+      console.log(gameBoardSqrs);
+    };
+
+    if (userGameOptionChoice === undefined && remeberGameMode2 !== undefined) {
+      startGameMode(`${remeberGameMode2}`);
+      console.log('Same mode: ' + remeberGameMode2)
+    } else {
+      remeberGameMode2 = userGameOptionChoice;
+      startGameMode(`${remeberGameMode2}`)
+      console.log('I changed mode ' + remeberGameMode2)
+    };
+  }
+
+  const addPlayerAndGameGrid = (userGameOptionChoice) => {
     const boardGrid = document.querySelectorAll('div');
     boardGrid.forEach((divs) => {
       if (divs.classList.contains('gameGrid0') || divs.classList.contains('gameGrid2')) {
@@ -89,12 +113,7 @@ const gameBoard = (() => {
         divs.appendChild(createRestartBtn);
 
       } else if (divs.classList.contains('gameGrid1')) {
-        for (let i = 1; i <= 9; i++) {
-          const createDiv = document.createElement('div');
-          createDiv.classList.add(`sqr${i}`);
-          divs.appendChild(createDiv);
-          addSqrs(`sqr${i}`);
-        };
+        createGameGrid(divs, userGameOptionChoice);
       };
     });
   };
@@ -116,12 +135,77 @@ const gameBoard = (() => {
     })
   };
 
+  const winConditions = (clicked) => {
+    const winMessage = document.createElement('p');
+    winMessage.classList.add('winMessage');
+    winMessage.textContent = 'Winner is player!'
+    // gameContainer.appendChild(winMessage);
+    const selectPlayersScore = document.querySelectorAll('.playerP2');
+    const boardGrid = document.querySelectorAll('div');
+
+    const resetGameBoard = () => {
+      let n = 1;
+      if (n === 10) {
+        n = 1;
+      };
+      boardGrid.forEach((divs) => {
+        if (divs.classList.contains(`sqr${n}`)) {
+          deleteGameGrid(divs);
+          gameBoardSqrs.splice(0);
+          n++;
+        };
+      });
+      boardGrid.forEach((divs) => {
+        if (divs.classList.contains('gameGrid1')) {
+          createGameGrid(divs);
+        };
+      });
+    };
+
+    if ((gameBoardSqrs[0].playerSqr && gameBoardSqrs[1].playerSqr  &&gameBoardSqrs[2].playerSqr === true) || (gameBoardSqrs[3].playerSqr && gameBoardSqrs[4].playerSqr  && gameBoardSqrs[5].playerSqr === true) || (gameBoardSqrs[6].playerSqr && gameBoardSqrs[7].playerSqr  && gameBoardSqrs[8].playerSqr === true) || (gameBoardSqrs[0].playerSqr && gameBoardSqrs[4].playerSqr  && gameBoardSqrs[8].playerSqr === true) || (gameBoardSqrs[2].playerSqr && gameBoardSqrs[4].playerSqr  && gameBoardSqrs[6].playerSqr === true) || (gameBoardSqrs[0].playerSqr && gameBoardSqrs[3].playerSqr  && gameBoardSqrs[6].playerSqr === true) || (gameBoardSqrs[1].playerSqr && gameBoardSqrs[4].playerSqr  && gameBoardSqrs[7].playerSqr === true) || (gameBoardSqrs[2].playerSqr && gameBoardSqrs[5].playerSqr  && gameBoardSqrs[8].playerSqr === true)) {
+      const selectGameBoardGrid = document.querySelector('.gameGrid1');
+      selectGameBoardGrid.style.pointerEvents = 'none';
+      selectPlayersScore.forEach((playerScore) => {
+        if (playerScore.parentElement.classList.contains('gameGrid0')) {
+          let newScore = players[0].score + 1;
+          players[0].score = newScore;
+          playerScore.textContent = players[0].score;
+          resetGameBoard();
+          return;
+        }
+      })
+    } else if ((gameBoardSqrs[0].player2Sqr && gameBoardSqrs[1].player2Sqr  &&gameBoardSqrs[2].player2Sqr === true) || (gameBoardSqrs[3].player2Sqr && gameBoardSqrs[4].player2Sqr  && gameBoardSqrs[5].player2Sqr === true) || (gameBoardSqrs[6].player2Sqr && gameBoardSqrs[7].player2Sqr  && gameBoardSqrs[8].player2Sqr === true) || (gameBoardSqrs[0].player2Sqr && gameBoardSqrs[4].player2Sqr  && gameBoardSqrs[8].player2Sqr === true) || (gameBoardSqrs[2].player2Sqr && gameBoardSqrs[4].player2Sqr  && gameBoardSqrs[6].player2Sqr === true) || (gameBoardSqrs[0].player2Sqr && gameBoardSqrs[3].player2Sqr  && gameBoardSqrs[6].player2Sqr === true) || (gameBoardSqrs[1].player2Sqr && gameBoardSqrs[4].player2Sqr  && gameBoardSqrs[7].player2Sqr === true) || (gameBoardSqrs[2].player2Sqr && gameBoardSqrs[5].player2Sqr  && gameBoardSqrs[8].player2Sqr === true)) {
+      const selectGameBoardGrid = document.querySelector('.gameGrid1');
+      selectGameBoardGrid.style.pointerEvents = 'none';
+      selectPlayersScore.forEach((playerScore) => {
+        if (playerScore.parentElement.classList.contains('gameGrid2')) {
+          let newScore = players[1].score + 1;
+          players[1].score = newScore;
+          playerScore.textContent = players[1].score;
+          resetGameBoard();
+          return;
+        }
+      })
+    } else {
+      console.log('Nobody wins!? (sadFace)')
+    }
+
+  }
+
   const startGameMode = (userGameOptionChoice) => {
+
+    const selectGameBoardGrid = document.querySelector('.gameGrid1');
+    selectGameBoardGrid.style.pointerEvents = 'all';
+
     let player1status = false;
     let player2status = true;
     let n = 1;
-    
-    const gameModePvp = (clicked) => { 
+
+    if (n === 10) {
+      n = 1;
+    };
+
+    const gameModePvp = (clicked) => {
       for (let n2 = 0; n2 < gameBoardSqrs.length; n2++) {
         if (player1status === false && gameBoardSqrs[`${n2}`].sqrStatusLocked === false && clicked.target.classList[0] === gameBoardSqrs[`${n2}`].id) {
           gameBoardSqrs[`${n2}`].playerSqr = true;
@@ -133,7 +217,7 @@ const gameBoard = (() => {
           addXToSqr.classList.add('fas');
           addXToSqr.classList.add('fa-times');
           selectGameSqrs.appendChild(addXToSqr);
-          console.dir(gameBoardSqrs);
+          winConditions(clicked);
         } else if (player2status === false && gameBoardSqrs[`${n2}`].sqrStatusLocked === false && clicked.target.classList[0] === gameBoardSqrs[`${n2}`].id) {
           gameBoardSqrs[`${n2}`].player2Sqr = true;
           gameBoardSqrs[`${n2}`].sqrStatusLocked = true;
@@ -144,14 +228,15 @@ const gameBoard = (() => {
           addOToSqr.classList.add('far');
           addOToSqr.classList.add('fa-circle');
           selectGameSqrs.appendChild(addOToSqr);
-          console.dir(gameBoardSqrs);
+          winConditions(clicked);
         };
       };
     };
-    
+
     const selectGameSqrs = document.querySelectorAll('div');
+
     selectGameSqrs.forEach((sqrs) => {
-      if (sqrs.classList.contains(`sqr${n}`)) {  
+      if (sqrs.classList.contains(`sqr${n}`)) {
         sqrs.addEventListener('click', (clicked) => {
           console.log(clicked.target.classList[0]);
           if (userGameOptionChoice === 'gameModePvp') {
@@ -164,12 +249,10 @@ const gameBoard = (() => {
         });
         n++;
       };
+
     });
+
   };
-
-  const winConditions = () => {
-
-  }
 
   return {
     initializeGame,
